1868fb2cc88973733a29e9192380542b511d4b8d
John Sigmon <johnasigmon@gmail.com>
2016-08-04 12:12:55 -0400

    add api files

--- server.js
+++ server.js
@@ -3,29 +3,19 @@ const express         = require('express');
 const path            = require('path');
 const logger          = require('morgan');
 const bodyParser      = require('body-parser');
-
 const methodOverride  = require('method-override');
-
 const homeRoute       = require('./controllers/homeRouter');
-
-
 
 const app             = express();
 const port            = process.env.PORT || 3000;
 
 
-
-
-
-app.use(methodOverride('_method'));
-
 app.set('view engine', 'ejs');
 
+app.use(methodOverride('_method'));
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
-
 app.use(express.static(path.join(__dirname,'public')));
-app.use('/bower_components', express.static(path.join(__dirname,'/bower_components')));
 
 app.use('/', homeRoute);
 
